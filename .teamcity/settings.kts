f2edd58839ceb7ad5a0932ccedf4957318c6307b
Arun <Arun@TeamCity>
2025-08-18 16:41:25 +0000

    runners of 'Build' build configuration were updated (TeamCity change in 'Teamcity2 (1)' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -36,6 +37,12 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetBuild {
+            id = "dotnet"
+        }
     }
 
     triggers {
